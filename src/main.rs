445f0940bc445bf8fd3a7eef72326111c66148a2
Piet Helzel <piet.helzel@outlook.com>
2022-10-18 23:16:24 +0200

    Basic example showing visualisation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod ast;
 mod bitcartesiann;
 mod parser;
+mod visualise;
 
 use parser::*;
+use visualise::visualise;
 
 fn main() {
-    println!("Hello, world");
+    let i = "(a&b)|((!a)&(!b))";
+
+    let parsed_expr = parse(i).unwrap();
+
+    println!("{parsed_expr}");
+
+    let eval_expr = parsed_expr.evaluate_all().unwrap();
+
+    visualise(eval_expr, i);
 }

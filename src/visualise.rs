bc95a9485b483fb2399a55cd9dd0e223425426a6
Piet Helzel <piet.helzel@outlook.com>
2022-10-20 07:53:02 +0200

    Added colored text output for bool values

--- src/visualise.rs
+++ src/visualise.rs
@@ -1,6 +1,9 @@
 use indexmap::IndexMap;
 
-use prettytable::{Table, Row, Cell, row};
+use prettytable::{Table, Row, Cell};
+use prettytable::format::Alignment;
+
+use colored::*;
 
 pub fn visualise(expr: Vec<(IndexMap<String, bool>, bool)>, input: &str) {
     if expr.is_empty() {
@@ -19,12 +22,21 @@ pub fn visualise(expr: Vec<(IndexMap<String, bool>, bool)>, input: &str) {
     for (map, result) in expr {
         let mut r = Row::empty();
         for c in map.values() {
-            r.add_cell(Cell::new(&c.to_string()));
+            //r.add_cell(Cell::new(&c.to_string()));
+            r.add_cell(Cell::new_align(&colorise(*c), Alignment::CENTER));
         }
-        r.add_cell(Cell::new(&result.to_string()));
+        r.add_cell(Cell::new_align(&colorise(result), Alignment::CENTER));
 
         table.add_row(r);
     }
 
     table.printstd();
+}
+
+fn colorise(a: bool) -> String {
+    if a {
+        (a as u8).to_string().green().to_string()
+    } else {
+        (a as u8).to_string().red().to_string()
+    }
 }
